993a737f67de03de1720d7008d5e0abeba341257
Dell <Dell@DESKTOP-TK7GSO1>
2026-03-18 18:34:26 -0300

    feat: implement authentication controller

---
+++ Auth10Api/Presentation/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Auth10Api.Application.Common;
+using Auth10Api.Application.Dtos;
+using Auth10Api.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth10Api.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IUserService _service;
+    private readonly ITokenService _tokenService;
+
+    public AuthController(IUserService service, 
+                          ITokenService tokenService)
+    {
+        _service = service;
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("[Action]")]
+    public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
+    {
+        var user = await _service.GetDataLoginAsync(userLoginDto);
+
+        if (user is null) return Unauthorized();
+
+        var token = _tokenService.GenerateToken(user);
+
+        return Ok(Result<Task<string>>.Ok(token));
+    }
+}
